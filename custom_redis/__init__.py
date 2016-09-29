cecae2edfca29262a2643c3a5665e24e1f07114b
shichaoma <308299269@qq.com>
2016-09-29 15:26:26 +0800

    修改setup bug

--- custom_redis/__init__.py
+++ custom_redis/__init__.py
@@ -1,16 +1,3 @@
 # -*- coding:utf-8 -*-
 from client import start_client
 from server import start_server
-
-
-VERSION = '1.0.7'
-
-AUTHOR = "cn"
-
-AUTHOR_EMAIL = "308299269@qq.com"
-
-URL = "https://www.github.com/ShichaoMa/custom_redis"
-
-NAME = "custom-redis"
-
-DESCRIPTION = "python实现简单redis，实现redis基本功能以及可插拔数据结构。 "
--- setup.py
+++ setup.py
@@ -6,7 +6,18 @@ try:
 except:
     from distutils.core import setup
 
-from custom_redis import AUTHOR, AUTHOR_EMAIL, VERSION, URL, NAME, DESCRIPTION
+
+VERSION = '1.0.8'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "308299269@qq.com"
+
+URL = "https://www.github.com/ShichaoMa/custom_redis"
+
+NAME = "custom-redis"
+
+DESCRIPTION = "python实现简单redis，实现redis基本功能以及可插拔数据结构。 "
 
 
 def read(fname):
