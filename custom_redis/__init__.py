ab1b67f2d28560888916c0c35e7c0cc39d1b45a9
ShichaoMa <270515529@qq.com>
2016-11-07 20:50:45 +0800

    增加版本信息

--- custom_redis/__init__.py
+++ custom_redis/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding:utf-8 -*-
 from client import start_client
 from server import start_server
+
+
+VERSION = '1.0.9'
+
+AUTHOR = "cn"
+
+AUTHOR_EMAIL = "308299269@qq.com"
+
+URL = "https://www.github.com/ShichaoMa/custom_redis"
+
